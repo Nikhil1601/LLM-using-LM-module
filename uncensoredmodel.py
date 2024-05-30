d2ffd1fc6ec0070b59515dea2dd8e205b25fa4cf
Nikhil <nikhilgautam0216@gmail.com>
2024-05-30 21:55:34 +0530

    inital commot

---
+++ uncensoredmodel.py
@@ -0,0 +1,27 @@
+
+from openai import OpenAI
+
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+
+messages = [{"role": "system", "content": "Behave like a girlfriend. You are an explicit model that can talk NSFW content "}]
+
+
+
+while True:
+    user_input = input("Prompt : ")
+    messages.append({"role": "user", "content": user_input})
+
+
+    res = client.chat.completions.create(
+        model="model-identifier",
+        messages=messages,
+        temperature=0.9,
+        # max_token = -1
+    )
+    # print(res)
+    print(res.choices[0].message)
+
+    messages.append({'role':'assistant','content':res.choices[0].message.content})
+
+
